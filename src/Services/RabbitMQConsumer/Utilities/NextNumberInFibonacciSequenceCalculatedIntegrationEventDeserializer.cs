caa9c037745d5791476e25186f5eb9df75214f36
RFanil <79217485+KFanil@users.noreply.github.com>
2023-03-08 23:29:26 +0300

    Minor change in NextNumberInFibonacciSequenceCalculatedIntegrationEventDeserializer

--- src/Services/RabbitMQConsumer/Utilities/NextNumberInFibonacciSequenceCalculatedIntegrationEventDeserializer.cs
+++ src/Services/RabbitMQConsumer/Utilities/NextNumberInFibonacciSequenceCalculatedIntegrationEventDeserializer.cs
@@ -7,14 +7,10 @@ namespace RabbitMQProducer.Utilities {
             var ind = json.IndexOf(",", startIndex);
             @event.Number = int.Parse(json.AsSpan(startIndex, ind - startIndex));
 
-            Guid guid = Guid.Empty;
             startIndex = ind + 11; // where guid starts
             int guidLength = 32 + 4;
-            var ttt = json.IndexOf('3');
-            var testNumber = json.AsSpan(startIndex, guidLength).ToString();
             @event.Id = Guid.Parse(json.AsSpan(startIndex, guidLength));
 
-            DateTime dateTime = DateTime.MinValue;
             startIndex = startIndex + guidLength + 23; // where date starts
             int dateLength = 27;
             @event.CreationDate = DateTime.Parse(json.AsSpan(startIndex, dateLength));
